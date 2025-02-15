e6a932464cef470cfcf8478b324bd6506635bc27
VladOS <diskopower@yandex.ru>
2025-02-15 13:13:12 +0300

    Исправлен тест

--- tests/test-app.py
+++ tests/test-app.py
@@ -1,4 +1,8 @@
-from app import app  # Импортируем приложение Flask
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app  # Теперь импорт должен работать
 
 def test_example():
     """Пример теста."""
